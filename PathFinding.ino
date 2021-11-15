1399789cd97b6bc126cd274018bc815c6467bb85
SilasStensgaard <silasstensgaard@gmail.com>
2021-11-15 12:53:16 +0100

    Update PathFinding.ino
    Working pathfinding

--- PathFinding.ino
+++ PathFinding.ino
@@ -1,19 +1,27 @@
-#include <Wire.h>
-#include <Zumo32U4.h>
-#include <math.h>
-Zumo32U4Motors motors;
-Zumo32U4Encoders encoders;
+
 int i = 0;
-int x,y;
+int maxY = 10;
+int x2,y2;
 int wildOats[2][7] = {{1,2,3,4,5,6,7},{2,5,7,9,4,6,3}};
-int currentPose[2] = {x,y};
+int currentPose[2] = {x2,y2};
 int moveThis[2] = {0,0};
+int returnToX[2] = {currentPose[0],0}; //Return to x-axis
+int returnToY[2] = {currentPose[0], maxY}; //Return to the the maximum y-value on the field
 
 void whereTo(){
   moveThis[0] = wildOats[0][i] - currentPose[0];
   moveThis[1] = wildOats[1][i] - currentPose[1];
   currentPose[0] = wildOats[0][i];
   currentPose[1] = wildOats[1][i];
+}
+
+void returning(){
+  if (currentPose[1] < maxY/2){
+    //drive to returnToX[1] on the y-axis
+  }
+  else{
+   // drive to returnToY[1]
+  }
 }
 
 void setup() {
@@ -27,6 +35,8 @@ void loop() {
     whereTo();
     Serial.println((String)currentPose[0] +" og "+ (String)currentPose[1]);
     delay(1000);
+    // kør først x og så y
+    returning();
   }
 
 }
