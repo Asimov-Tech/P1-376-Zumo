10c7a97ee0e78215ba0bbc25aae67ef08b4eb4d0
SilasStensgaard <silasstensgaard@gmail.com>
2021-11-15 10:32:05 +0100

    Update of Location_in_coordinatesystem_with_movement
    Jeg har samlet alle funktioner i en samlet  funktion og lavet kommentarer

--- Location_in_coordinatesystem_with_movement.ino
+++ Location_in_coordinatesystem_with_movement.ino
@@ -1,25 +1,30 @@
 #include <Wire.h>
 #include <Zumo32U4.h>
 #include <math.h>
-double x = 0; //the x coordinate
-double y = 0; //the y coordinate
-double pose = 0; //the position angle compared to the x-axis given in radians
-double deg = 0; //For cenverting radians to degrees 
-double temp;
-
-double distRight = 10; //the dist traveled on the right wheel (right and left distance will always have to come for the same amount of time)
-double distLeft = 7; //the dist traveled on the left wheel
-//these should be found with the encoders 
-double distBetweenWheels = 4;  //4 cm between each wheel
+Zumo32U4Motors motors;
+Zumo32U4Encoders encoders;
+double x = 0, y = 0, pose = 0, temp, deg = 0; //the x coordinate, the y coordinate, the position angle compared to the x-axis given in radians, "deg" For cenverting radians to degrees 
+//Distance on the right and left will always come from an equal amount of time
+double distRight, distLeft; //the dist traveled on the right wheel, dist traveled on left wheel, counts from 
 
 double poseVector[3] = {0,0,0};  // The "beginning" vector 
 double newPose[3]; //A "new position" vector for new coordinates and angles from the distance traveled given that the starting position is [0,0,0]
+int counts[2]; //an array for the numbers retrieved from the encoders, not really needed here, but maybe in some other part of the code idk
+
+void getEncoderAndDist() {
+  counts[0] = encoders.getCountsAndResetRight(); //Getting the counts from each encoder
+  counts[1] = encoders.getCountsAndResetLeft();
+  distRight = (1/78.5)*counts[0]; // Using these counts to calculate the distance each wheel has traveled 
+  distLeft = (1/78.5)*counts[1]; // 1/78.5 is the ratio between a centimeter and 1 encoder count
+  return counts;
+}
 
 void calCoordiPose(){
   double poseNew = 0; //Values for the "new position" vector
   double xNew = 0;
   double yNew = 0;
   double distTotal = 0; //Sort of a total distance traveled, but not really
+  double distBetweenWheels = 8.5;  //4 cm between each wheel
   poseNew = (distRight-distLeft)/distBetweenWheels; //calculate the new position angle after the wheels have moved their distance, given in radians
   distTotal = (distRight+distLeft)/2; //calculating total distance so that a nex x and y can be calculated
   xNew = distTotal*cos(poseVector[2]+(poseNew/2)); //calculate the new x-coordinate after move
@@ -33,9 +38,14 @@ void calCoordiPose(){
   y = poseVector[1] + newPose[1]; // adding new y-value to the old one
   temp = poseVector[2] + newPose[2]; // adding new pose-value to the old one (calling it "temp" because otherwise it wasn't working)
    //These new values are now the values of the next "beginning" vector
+
+   //The next two if-statements make sure that when the rotation angel exceeds 180 or goes below -180 degrees, it will "reset" it
   if (temp > M_PI){
     // pose = pose%M_PI; (If it spins more than 180 degrees in a single go, which it won't if we keep the amount of time to a minimum)
     temp = - M_PI + (temp - M_PI);
+  }
+  if (temp < -M_PI){
+    temp = M_PI -(temp + M_PI);
   }
   poseVector[0] =x; //Each new value is given to the position vector
   poseVector[1] =y;
@@ -47,16 +57,30 @@ void convertRadians(){
   return deg;
 }
 
+void location(){ //function compiling all the other functions
+  getEncoderAndDist(); //Getting the distance each wheel has traveled from the encoder counts 
+  calCoordiPose(); //using those distances to calculate the coordinates
+  convertRadians(); // converting the radians to degrees for the print
+  Serial.println("New x-value: " + (String)poseVector[0] + " New y-value: " + (String)poseVector[1] + " New position: " + (String)deg + " degrees");
+  delay(50); //Reading distances in intervals of 50 milliseconds
+
+}
+
+
+
 void setup() {
   Serial.begin(9600);
 }
+//bool testing = true;
 
 void loop() {
-  calCoordiPose();
-  delay(200);
-  convertRadians();
-  delay(200);
-  Serial.println("New x-value: " + (String)poseVector[0] + " New y-value: " + (String)poseVector[1] + " New position: " + (String)deg + " degrees");
-  delay(5000); //Should do it quicker
+  location();
+ // delay(3000);
+ // motors.setSpeeds(50,50);
+ // while (testing == true){
+ // if ((poseVector[0]+0.4) >= 10){
+ //   testing = false;
+ // }
+  }
+  //motors.setSpeeds(0,0);
   
-}
