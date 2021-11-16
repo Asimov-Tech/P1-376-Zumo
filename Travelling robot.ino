3c199a89dff9a3e2b972789e8a11bc031082a6b9
Hans Henrik Dalgaard <hans.henrik.dalgaard@gmail.com>
2021-11-16 14:33:54 +0100

    Added comments and removed the void loop

--- Travelling robot.ino
+++ Travelling robot.ino
@@ -1,24 +1,26 @@
 #include <Wire.h>
-//#include <Zumo32U4.h>
+#include <Zumo32U4.h>
 
 
 
-const int numDest =7;//7 points takes approx. 5 minutes
-
+const int numDest =7; //amount of destinations
 struct Destination{
-    char id;
+    char id; //character to identify the destination8
     int x;//x-coordinate
     int y;//y-coordinate
 };
 
+
 struct Route{
-    char stops[numDest];
+    char stops[numDest];//Ordered array containing the array
     double dist;
 };
 
+//High number so that all routes are shorter
 Route shortestRoute = {{},1000000000};
 
 
+//Destinations with coordinates
 Destination spots[numDest] ={
     {'A', 0, 0},
     {'B', 4, 1},
@@ -30,6 +32,7 @@ Destination spots[numDest] ={
 };
 
 char destinationsID[numDest];
+//Sets up an array of numbers. Is used in the permutation
 void setupDestinations(){
     for(int i=0; i < numDest; i++)
         destinationsID[i]=i; 
@@ -37,14 +40,18 @@ void setupDestinations(){
 
 
 double costMatrix[numDest][numDest];
-void setupCostMatrix(){
-    for(int i=0; i < numDest; i++){//Choose row first
-        for(int j=0; j < numDest; j++){//choose column second
-            costMatrix[i][j]=sqrt(pow(spots[i].x-spots[j].x,2)+pow(spots[i].y-spots[j].y,2)); //calculates the distance as sqrt(x^2+y^2)
-        }
+//Sets up a cost matrix with direct routes between points
+void setupCostMatrix()
+{   //Chooses row first
+    for(int i=0; i < numDest; i++)
+    {   //chooses column second
+        for(int j=0; j < numDest; j++)
+        {   //calculates the distance as sqrt(x^2+y^2)
+            costMatrix[i][j]=sqrt(pow(spots[i].x-spots[j].x,2)+pow(spots[i].y-spots[j].y,2)); 
+            }
     }
 }
-
+//Prints the cost matrix in the Serial moniter. for debugging 
 void printCostMatrix(){
     Serial.println("    A     B     C     D     E     F     G");
     for(int i=0; i < numDest; i++){//Choose row first
@@ -59,13 +66,14 @@ void printCostMatrix(){
     }   
 }
 
+//Uses all setup functions. Important to use the travelling robot code
 void routeInit(){
     setupDestinations();
     setupCostMatrix();
     printCostMatrix();
 }
 
-
+//function to swap 2 index positions in the destinationsID array
 void swapDestinations(int i, int j){
     char pH = destinationsID[i];
     destinationsID[i]=destinationsID[j];
@@ -75,48 +83,47 @@ void swapDestinations(int i, int j){
 
 
 
-void permuteDestinations(int l, int r){
-    if(l==r){
+void permuteDestinations(int l, int r)
+{
     //if no more permutation options print the permutation
+    if(l==r)
+    {
+        //Distance of current route
         double dist = 0;
-        for(int i = 0; i<r;i++){
+        //Prints the route
+        for(int i = 0; i<r;i++)
+        {
             Serial.print(spots[destinationsID[i]].id);
+            // checks whether there are any stops left, and if there are adds the distance from current point to next
             if(i<r-1) dist += costMatrix[destinationsID[i]][destinationsID[i+1]];
         }
+        //print the distance of the route after the route
         Serial.print(" - Distance: " + (String)dist);
-        if(dist < shortestRoute.dist){
-            for(int i = 0; i<r;i++){
+        //Checks whether current route is shorter than the previous shortes route
+        if(dist < shortestRoute.dist)
+        {
+            //changes the point order if it is the case
+            for(int i = 0; i<r;i++)
+            {
                 shortestRoute.stops[i] = spots[destinationsID[i]].id;
             }
+            //changes the distance to be the new one
             shortestRoute.dist = dist;
-            Serial.print(" New shortest!");
+            Serial.print(". New shortest!");
         }
         Serial.println();
     } else {
         for(int i = l; i < r; i++){
+            //Swaps first the l'th index with the i'th
             swapDestinations(l,i);
-            //lock first selection (most cases swap)
+            //reiterates the function but with the just swapped and all previous indexes locked
             permuteDestinations(l+1,r);
-            //do again
+            //functionally returns the ID's to the initial position
             swapDestinations(l,i);
-            //backtrack so we can start over
+            
         }
     }
 
 }
 
 
-
-void setup(){
-  delay(200);
-    Serial.begin(9600);
-    routeInit();
-}
-
-void loop(){
-    permuteDestinations(0,numDest);
-    Serial.print("Shortest route is: ");
-    for(int i=0;i<numDest;i++) Serial.print(shortestRoute.stops[i]);
-    Serial.println(" with a distance of " + (String)shortestRoute.dist);
-    delay(30000);
-}
